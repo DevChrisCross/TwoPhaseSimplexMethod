8698f9b289b9c25405b678fc4479c61ae7ccdd89
Emmanuel Vergara <emmanuel.vergara@tup.edu.ph>
2017-10-04 00:21:55 +0800

    Added an animation to focus on the Solution Container when the user pressed "Compute"

--- website/static/js/styles.js
+++ website/static/js/styles.js
@@ -102,5 +102,8 @@ for (i = 0; i < acc.length; i++) {
   }
 } 
 
-
-// Generating Constraints and Objectives
+$("#computeBtn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#solCont").offset().top
+    }, 2000);
+});
